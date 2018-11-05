e6193b9e8334a6b974dbe4e42371dc2540b9ce7f
PARK Yong Seo <j@saro.me>
2018-11-05 22:59:11 +0900

    add test class

--- src/test/java/me/saro/sap/jco/test/SapUtilsTest.java
+++ src/test/java/me/saro/sap/jco/test/SapUtilsTest.java
@@ -2,10 +2,28 @@ package me.saro.sap.jco.test;
 
 import org.junit.jupiter.api.Test;
 
+import me.saro.sap.jco.SapField;
+
 public class SapUtilsTest {
     
     @Test
     public void convertField() {
         
+    }
+    
+    
+    public static class User {
+        
+        @SapField("FIRST_NAME")
+        String firstName;
+        
+        @SapField("LAST_NAME")
+        String lastName;
+        
+        @SapField("BIRTH")
+        String birth;
+        
+        @SapField("JOIN_DATE")
+        String joinDate;
     }
 }
